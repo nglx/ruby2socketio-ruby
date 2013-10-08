7400275b37b9a2f3046fbe624a601b4f0ede53ce
Marcin Naglik <marcin.naglik@gmail.com>
2013-10-08 23:14:02 +0200

    redis from heroku

--- pub.rb
+++ pub.rb
@@ -1,7 +1,15 @@
 require 'redis'
 require 'json'
 
-REDIS = Redis.new
+if ENV["REDISCLOUD_URL"]
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+elsif ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+else
+  REDIS = Redis.new
+end
 
 loop do
   out = Time.now.to_json
